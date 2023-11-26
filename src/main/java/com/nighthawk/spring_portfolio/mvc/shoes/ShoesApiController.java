94b38b2ffef940d98caf5f4c094e78aae6be71a8
Haoxuan Tong <haoxuantong8@gmail.com>
2023-11-25 23:59:38 -0800

    deleted:    src/main/java/com/nighthawk/spring_portfolio/mvc/names/Names.java 	new file:   src/main/java/com/nighthawk/spring_portfolio/mvc/shoes/Shoes.java 	renamed:    src/main/java/com/nighthawk/spring_portfolio/mvc/names/JokesApiController.java -> src/main/java/com/nighthawk/spring_portfolio/mvc/shoes/ShoesApiController.java 	renamed:    src/main/java/com/nighthawk/spring_portfolio/mvc/names/JokesJpaRepository.java -> src/main/java/com/nighthawk/spring_portfolio/mvc/shoes/ShoesJpaRepository.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/shoes/ShoesApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/shoes/ShoesApiController.java
@@ -1,4 +1,4 @@
-package com.nighthawk.spring_portfolio.mvc.jokes;
+package com.nighthawk.spring_portfolio.mvc.shoes;   
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController // annotation to simplify the creation of RESTful web services
-@RequestMapping("/api/jokes")  // all requests in file begin with this URI
+@RequestMapping("/api/shoes")  // all requests in file begin with this URI
 public class JokesApiController {
 
     // Autowired enables Control to connect URI request and POJO Object to easily for Database CRUD operations
@@ -29,22 +29,29 @@ public class JokesApiController {
      * @PutMapping annotation is used for mapping HTTP PUT requests onto specific handler methods.
      * @PathVariable annotation extracts the templated part {id}, from the URI
      */
-    @PostMapping("/like/{id}")
+    @Getmapping("/name")
     public ResponseEntity<Jokes> setLike(@PathVariable long id) {
         /* 
         * Optional (below) is a container object which helps determine if a result is present. 
         * If a value is present, isPresent() will return true
         * get() will return the value.
         */
-        Optional<Jokes> optional = repository.findById(id);
-        if (optional.isPresent()) {  // Good ID
-            Jokes joke = optional.get();  // value from findByID
-            joke.setHaha(joke.getHaha()+1); // increment value
-            repository.save(joke);  // save entity
-            return new ResponseEntity<>(joke, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
-        }
-        // Bad ID
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);  // Failed HTTP response: status code, headers, and body
+        Shoes names = repository.findAll()
+        int i, key, j;
+        for (i = 1; i < n; i++) {
+            key = arr[i];
+            j = i - 1;
+    
+            // Move elements of arr[0..i-1],
+            // that are greater than key, 
+            // to one position ahead of their
+            // current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        return names;
     }
 
     /* Update Jeer

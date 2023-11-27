54f22add1e1012c825c5d979ce1922920a50b4c2
aaron-rub <aaronr06138@gmail.com>
2023-11-27 11:59:59 -0800

    Change- Aaron

--- src/main/java/com/nighthawk/spring_portfolio/mvc/price/JokesApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/price/JokesApiController.java
@@ -1,4 +1,4 @@
-package com.nighthawk.spring_portfolio.mvc.jokes;
+package com.nighthawk.spring_portfolio.mvc.price;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- src/main/java/com/nighthawk/spring_portfolio/mvc/price/JokesJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/price/JokesJpaRepository.java
@@ -1,4 +1,4 @@
-package com.nighthawk.spring_portfolio.mvc.jokes;
+package com.nighthawk.spring_portfolio.mvc.price;
 
 import java.util.List;
 

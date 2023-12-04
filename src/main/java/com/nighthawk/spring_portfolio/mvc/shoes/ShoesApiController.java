1317df394d23c4b8de22b8b4c24eb5449c4e565a
Haoxuan Tong <haoxuantong8@gmail.com>
2023-12-04 11:51:19 -0800

    modified:   src/main/java/com/nighthawk/spring_portfolio/mvc/shoes/ShoesApiController.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/shoes/ShoesApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/shoes/ShoesApiController.java
@@ -37,7 +37,8 @@ public class ShoesApiController {
         * get() will return the value.
         */
         Shoes names = repository.findAll()
-        int i, key, j;
+        int i, j;
+        Shoes key;
         for (i = 1; i < n; i++) {
             key = names[i];
             j = i - 1;
@@ -53,16 +54,21 @@ public class ShoesApiController {
     @GetMapping("/price")
     public ResponseEntity<Jokes> getName(@PathVariable long id) {
         Shoes price = repository.findAll()
-        int i, key, j;
-        for (i = 1; i < n; i++) {
-            key = names[i];
-            j = i - 1;
-    
-            while (j >= 0 && names[j].name > key.name) {
-                names[j + 1] = names[j];
-                j = j - 1;
+        int i, j;
+        Shoes temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (price[j].price > price[j + 1].price) {
+                    temp = price[j];
+                    price[j] = price[j + 1];
+                    price[j + 1] = temp;
+                    swapped = true;
+                }
             }
-            names[j + 1] = key;
-        return names;
+            if (swapped == false)
+                break;
+        }
     }
 }
